db2fce1c304ffd82d518070ae37f3d773c1fe9c3
ekkapX <ay.ivanoff2017@mail.ru>
2025-09-13 22:45:41 +0300

    task: L1 completed

--- 1/l1.go
+++ 1/l1.go
@@ -4,6 +4,26 @@ import (
 	"fmt"
 )
 
+type Human struct {
+	Name string
+	Age  int
+}
+
+type Action struct {
+	Human
+	ActionName string
+}
+
+func (h Human) Do() {
+	fmt.Println(h.Name + " doing something")
+}
+
+func (a Action) Info() {
+	fmt.Printf("His hame %s, he's %d years old and his action actually doing %s", a.Name, a.Age, a.ActionName)
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	action := Action{Human: Human{Name: "Dmitro", Age: 23}, ActionName: "nothing"}
+	action.Do()
+	action.Info()
 }
